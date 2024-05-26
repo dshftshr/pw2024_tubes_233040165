4fdb066974d58763139e4a0afef55b69a121ff98
dshftshr <desihafitaashri.dha@gmail.com>
2024-05-26 20:54:04 +0700

    edit

---
+++ Halaman Login/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Login</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+    <link rel="stylesheet" href="login.css" media="screen" title="no title">
+</head>
+<body>
+    <div class="input">
+        <h1>LOGIN</h1>
+        <div class="box-input">
+            <i class="fas fa-envelope-open-text"></i>
+            <input type="text" placeholder="Email">
+        </div>
+        <div class="box-input">
+            <i class="fas fa-lock"></i>
+            <input type="password" placeholder="Password"> 
+        </div>
+        <button type="submit" name="login" class="btn-input">Login</button>
+        <div class="bottom">
+            <p>Belum punya akun?
+                <a href="register.php">Register disini</a>
+            </p>
+
+        </div>
+        
+    </div>
+    
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".home", {
+    spaceBetwen: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
